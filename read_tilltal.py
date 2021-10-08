b2cab97ef70bfb2334eff4f7a2b37424e67a144b
mariask2 <maria.skeppstedt@lnu.se>
2021-10-08 22:22:52 +0200

    more statistics

--- read_tilltal.py
+++ read_tilltal.py
@@ -54,6 +54,10 @@ print()
 classification_dict = {}
 classification_statistics = {}
 classification_statistics_main = {}
+rubrik_label_statistics = {}
+rubrik_sub_labels_statistics = {}
+underrubrik_benamning_tuples_statistics = {}
+
 nr_of_classifications_list = []
 nr_of_sentence_list = []
 
@@ -64,6 +68,9 @@ for el in data:
 
     classifications = []
     main_classifications = []
+    rubrik_underrubrik_tuples = []
+    underrubrik_benamning_tuples = []
+    
     for classification in el["klassifikation"]:
         additional_info = []
         for name in ["Rubrik2", "Rubrik3", "Rubrik4", "Underrubrik", "Benämning"]:
@@ -78,13 +85,23 @@ for el in data:
         if len(classification_to_append.replace("[", "").replace("]","").replace(" ", "")) > 0:
             classifications.append(classification_to_append)
         
-        if classification["Huvudrubrik"]:
+        if classification["Huvudrubrik"] and classification["Huvudrubrik"] != "":
             main_classifications.append(classification["Huvudrubrik"])
     
+        if classification["Rubrik"] and classification["Rubrik"] != "":
+            rubrik_underrubrik_tuple = (classification["Rubrik"], classification["Underrubrik"])
+            if rubrik_underrubrik_tuple not in rubrik_underrubrik_tuples:
+                rubrik_underrubrik_tuples.append(rubrik_underrubrik_tuple)
+            if classification["Underrubrik"] != "":
+                underrubrik_benamning_tuple = (classification["Rubrik"] + "/" + classification["Underrubrik"], classification["Benämning"])
+                if underrubrik_benamning_tuple not in underrubrik_benamning_tuples:
+                    underrubrik_benamning_tuples.append(underrubrik_benamning_tuple)
+        
     main_classifications = set(main_classifications)
     
     main_folder = "tilltal"
     rubrik_labels = [c["Rubrik"].strip() for c in el["klassifikation"]]
+    
     
     infreq = get_most_infrequenct(rubrik_labels, categories) # Use the most infrequent category as the main one
     if infreq == "":
@@ -95,7 +112,7 @@ for el in data:
         os.makedirs(path)
     n = current_inspelning + "_" + str(inspelning_counter)
     classifications = list(set(classifications))
-    classification_dict[n] = classifications
+    #classification_dict[n] = classifications
     
     if n not in dupletter:
         nr_of_classifications_list.append(len(classifications))
@@ -111,8 +128,29 @@ for el in data:
             else:
                 classification_statistics_main[stat_cl_main] = classification_statistics_main[stat_cl_main] + 1
         
-                
-                
+        rubrik_already_added_for_text_list = []
+        for (r, u) in rubrik_underrubrik_tuples:
+            if r not in rubrik_already_added_for_text_list:
+                if r not in rubrik_label_statistics:
+                    rubrik_label_statistics[r] = 1
+                else:
+                    rubrik_label_statistics[r] = rubrik_label_statistics[r] + 1
+            rubrik_already_added_for_text_list.append(r)
+            if r not in rubrik_sub_labels_statistics:
+                rubrik_sub_labels_statistics[r] = {}
+            if u not in rubrik_sub_labels_statistics[r]:
+                rubrik_sub_labels_statistics[r][u] = 1
+            else:
+                rubrik_sub_labels_statistics[r][u] = rubrik_sub_labels_statistics[r][u] + 1
+        
+        for (ur, b) in underrubrik_benamning_tuples:
+            if ur not in underrubrik_benamning_tuples_statistics:
+                underrubrik_benamning_tuples_statistics[ur] = {}
+            if b not in underrubrik_benamning_tuples_statistics[ur]:
+                underrubrik_benamning_tuples_statistics[ur][b] = 1
+            else:
+                underrubrik_benamning_tuples_statistics[ur][b] = underrubrik_benamning_tuples_statistics[ur][b] + 1
+
     file_name = os.path.join(path, n + ".txt")
 
     to_file = open(file_name, "w")
@@ -128,6 +166,9 @@ f.close()
 #print(classification_dict)
 cs_list = sorted([(item, key) for (key, item) in classification_statistics.items()])
 cs_list_main = sorted([(item, key) for (key, item) in classification_statistics_main.items()])
+
+rubrik_label_statistics_list = sorted([(item, key) for (key, item) in rubrik_label_statistics.items()], reverse=True)
+
 
 at_least_five = 0
 for (key, item) in cs_list:
@@ -153,3 +194,19 @@ print("nr_of_classifications_list median", statistics.median(nr_of_classificatio
 print("nr_of_sentence_list mean", sum(nr_of_sentence_list)/len(nr_of_sentence_list))
 print("nr_of_sentence_list median", statistics.median(nr_of_sentence_list))
 
+for (nr, item) in rubrik_label_statistics_list:
+    if nr >= 20:
+        print("*****")
+        print(item, nr)
+        sub_cateogories = sorted([(item, key) for (key, item) in rubrik_sub_labels_statistics[item].items()], reverse=True)
+        for (sub_nr, sub_item) in sub_cateogories:
+            if sub_nr >= 10 and sub_item != "":
+                print("\t", sub_item, sub_nr)
+                benamning_cateogories = sorted([(ii, kk) for (kk, ii) in underrubrik_benamning_tuples_statistics[item + "/" + sub_item].items()], reverse=True)
+                for (benamning_nr, benamnming_item) in benamning_cateogories:
+                    if benamning_nr >= 10:
+                        print("\t\t", benamnming_item, benamning_nr)
+
+        
+
+#print(rubrik_sub_labels)
